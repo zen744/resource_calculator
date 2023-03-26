990a6e297486283e2e28aa24c722c5eccd1e3522
zen744 <84705276+zen744@users.noreply.github.com>
2023-03-26 10:10:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,47 +3,53 @@ function cal(){
     let stack_type = document.getElementById("stack_type").value
     let item = document.getElementById("item").value
     let stack = document.getElementById("stack").value
+    let sb = document.getElementById("sb").value
     let result = document.getElementById("result")
-    let temp = ""
-    let temp2 = ""
+    let item_op = ""
+    let stack_op = ""
+    let sb_op = ""
 
-    if(stack_type == "st16"){
-        if(cal_type == "sti"){
-            Number(stack)
-            stack = stack*16
-            result.innerHTML=stack+" item"
-            document.getElementById("item").value=""
+    // calculate item to stack and shulker box
+    if (cal_type == "its"){
+        // check how much itemn in 1 stack
+        if (stack_type == "st64"){
+            sb_op = parseInt(item/1728)
+            item = item-(sb_op*1728)
+            stack_op = parseInt(item/64)
+            item = item-(stack_op*64)
+            item_op = parseInt(item)
+            return(result.innerHTML = `${sb_op} shulker box |${stack_op} stack |${item_op} item`)
         }
-        else if(cal_type == "its"){
-            Number(item)
-            if(item < 16){
-                result.innerHTML=item+" item"
-            }
-            else{
-                temp = Math.floor(item/16)
-                temp2 = item%16
-                result.innerHTML=temp+" stack "+temp2+" item"
-            }
-            document.getElementById("stack").value=""
+        else if (stack_type == "st16"){
+            sb_op = parseInt(item/432)
+            item = item-(sb_op*432)
+            stack_op = parseInt(item/16)
+            item = item-(stack_op*16)
+            item_op = parseInt(item)
+            return(result.innerHTML = `${sb_op} shulker box |${stack_op} stack |${item_op} item`)
+        }
+        else {
+            // unstackable item don't need to calculate stack
+            sb_op = parseInt(item/27)
+            item = item-(sb_op*27)
+            item_op = parseInt(item)
+            return(result.innerHTML = `${sb_op} shulker box |${stack_op} stack |${item_op} item`)
         }
     }
-    else if(stack_type == "st64"){
-        if(cal_type == "sti"){
-            Number(stack)
-            stack = stack*64
-            result.innerHTML=stack+" item"
-            document.getElementById("item").value=""
+    // calculat shulker box and stack to item
+    else {
+        if (stack_type == "st64"){
+            item_op = (sb*1728)+(stack*64)
+            return(result.innerHTML = `${sb_op} shulker box |${stack_op} stack |${item_op} item`)
         }
-        else if(cal_type == "its"){
-            if(item < 64){
-                result.innerHTML=item+" item"
-            }
-            else{
-                temp = Math.floor(item/64)
-                temp2 = item%64
-                result.innerHTML=temp+" stack "+temp2+" item"
-            }
-            document.getElementById("stack").value=""
+        else if (stack_type == "st16"){
+            item_op = (sb*432)+(stack*16)
+            return(result.innerHTML = `${sb_op} shulker box |${stack_op} stack |${item_op} item`)
+        }
+        else {
+            // unstackable item don't need to calculate stack
+            item_op = sb*27
+            return(result.innerHTML = `${sb_op} shulker box |${stack_op} stack |${item_op} item`)
         }
     }
 }
